5bd7211d7a295b6613ce0d8ad276790c707bc4f3
Orsiris de Jong <ozy@netpower.fr>
2023-11-23 13:35:43 +0100

    Add encoding auto-detection

--- csv2vcard/csv_handler.py
+++ csv2vcard/csv_handler.py
@@ -2,16 +2,30 @@ import os
 from csv2vcard.export_vcard import check_export_dir, export_vcard
 from csv2vcard.create_vcard import create_vcard
 import csv
-import codecs
+try:
+    from charset_normalizer import detect
+    _NORMALIZER=True
+except ImportError:
+    print("We cannot automagically determine source file encoding. Please install charset_normalizer via pip")
+    _NORMALIZER=False
 
 
-def parse_csv(csv_filename: str, csv_delimiter: str):
+def parse_csv(csv_filename: str, csv_delimiter: str, encoding: str = None) -> dict:
     """
     Simple csv parser with a ; delimiter
     """
+
+    if not encoding and _NORMALIZER:
+        with open(csv_filename, 'rb') as fp:
+            # Read first MB of data
+            encoding = detect(fp.read(1024**3))["encoding"]
+        print(f"Guessed file encoding: {encoding}")
+    else:
+        encoding='utf-8'
+
     print("Parsing csv..")
     try:
-        with open(f"{csv_filename}", "r", encoding="utf-8") as fh:
+        with open(f"{csv_filename}", "r", encoding=encoding) as fh:
             contacts = csv.reader(fh, delimiter=csv_delimiter)
             header = next(contacts)  # saves header
             parsed_contacts = [dict(zip(header, row)) for row in contacts]
@@ -19,16 +33,19 @@ def parse_csv(csv_filename: str, csv_delimiter: str):
     except OSError as exc:
         print(f"OS error for {csv_filename}: {exc}")
         return []
+    except UnicodeDecodeError as exc:
+        print(f"Failed to decode file with encoding {encoding}. Try to adjust manually with --encoding parameter")
+        return []
 
 
-def csv2vcard(csv_filename: str, csv_delimiter: str = ';', output_dir: str = None, vcard_version: int = 4, single_vcard_file: bool = False) -> None:
+def csv2vcard(csv_filename: str, csv_delimiter: str = ';', encoding: str = None, output_dir: str = None, vcard_version: int = 4, single_vcard_file: bool = False) -> None:
     """
     Main function
     """
     check_export_dir(output_dir)
 
     vcards = ""
-    for contact in parse_csv(csv_filename, csv_delimiter):
+    for contact in parse_csv(csv_filename, csv_delimiter, encoding):
         vcard, filename = create_vcard(contact, vcard_version)
         if single_vcard_file:
             export_vcard(vcard, output_dir, filename)
