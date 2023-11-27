f2a9c7efeeba60ae13086335c7b20283514eb1d0
Orsiris de Jong <ozy@netpower.fr>
2023-11-27 17:15:15 +0100

    Update max_vcard_file_size widget on gui reload

--- csv2vcard/gui.py
+++ csv2vcard/gui.py
@@ -69,6 +69,9 @@ def update_gui_from_config(window: sg.Window, config: dict) -> bool:
     for gui_var, value in config["settings"].items():
         window[f"-{gui_var}-"].update(value)
 
+    # Special fix for max_vcard_file_size
+    window["-max_vcard_file_size-"].update(disabled=not config["settings"]["single_vcard_file"]])
+
     return True
 
 
